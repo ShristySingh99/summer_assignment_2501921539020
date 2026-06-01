e45fea113eca0a6fe7466002a1844e32050c65a9
Vidit Sharma <vidits852@gmail.com>
2026-06-01 13:50:27 +0530

    Completed Day 1 Assignment

---
+++ Week_1/Day_1/Two_Sum.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+#include <vector>
+#include <unordered_map>
+    vector<int> twoSum(vector<int> &nums, int target) {
+        int n= nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n; i++){
+            int sec = target-nums[i];
+            if(mp.find(sec)!= mp.end()){
+                int j= mp[sec];
+                return {i,j};
+            }
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
