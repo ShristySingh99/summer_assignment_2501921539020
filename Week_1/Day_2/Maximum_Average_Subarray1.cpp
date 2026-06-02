83a8c13daa8c698320a4a78c77007654dd518d7d
Shristy Singh <152967083+ShristySingh99@users.noreply.github.com>
2026-06-02 14:08:42 +0530

    Add Maximum Average Subarray solution
    Implement a solution to find the maximum average of a subarray of size k.

---
+++ Week_1/Day_2/Maximum_Average_Subarray1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n= nums.size();
+        double CurrSum=0;
+        for(int i=0; i<k;i++){
+            CurrSum+= nums[i];
+        }
+
+        double maxSum= CurrSum;
+        for(int i=k; i<n;i++){
+            CurrSum += nums[i]- nums[i-k];
+            maxSum= max(maxSum, CurrSum);
+        }
+        return maxSum/k;
+    }
+};
+
