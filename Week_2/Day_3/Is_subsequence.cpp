b226e61efd1451e278300206ad32d9740318d7f2
Shristy Singh <152967083+ShristySingh99@users.noreply.github.com>
2026-06-10 13:58:45 +0530

    Implement isSubsequence function in Is_subsequence.cpp

---
+++ Week_2/Day_3/Is_subsequence.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n= s.length();
+        int m= t.length();
+        int idx=0;
+        for(int i=0;i<m; i++){
+            if(t[i]== s[idx]) idx++;//count number of character matched in s
+        }
+        return idx==n;
+    }
+};
