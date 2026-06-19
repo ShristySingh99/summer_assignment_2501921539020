f5bc011afbde3fdbfea484c5a25c2bbb7c3d940b
Shristy Singh <152967083+ShristySingh99@users.noreply.github.com>
2026-06-19 07:38:14 +0530

    Implement daily temperature calculation in Daily_temp.cpp

---
+++ Week_3/Day_4/Daily_temp.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n= temperatures.size();
+        stack<int>st;
+        vector<int>temp(n,0);
+        for(int i=n-1; i>=0; i--){
+            while(!st.empty() && temperatures[i]>= temperatures[st.top()]){
+                st.pop();
+            }
+            if(!st.empty()) temp[i]= st.top()-i;
+            st.push(i);
+        }
+        return temp;
+    }
+};
