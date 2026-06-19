b5c428f34ecad7a1d5b09fbbf8e58f5bb2703a0a
Shristy Singh <152967083+ShristySingh99@users.noreply.github.com>
2026-06-19 12:11:36 +0530

    Implement RecentCounter class with ping method

---
+++ Week_3/Day_5/no_of_recent_calls.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+class RecentCounter {
+public:
+    queue<int>q;
+    RecentCounter() {
+        //initialization
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        while(!q.empty() && q.front()< t-3000){
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
